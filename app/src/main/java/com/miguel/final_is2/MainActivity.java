52cc0a11f2579c2da9c2b772752e6ef285c78250
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-10-21 17:20:19 -0500

    la estructura de la página principal está lista y la acción de cambio de pantalla sirve

--- app/src/main/java/com/miguel/final_is2/MainActivity.java
+++ app/src/main/java/com/miguel/final_is2/MainActivity.java
@@ -2,13 +2,30 @@ package com.miguel.final_is2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button parati;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parati = findViewById(R.id.para_ti);
+
+        parati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pt = new Intent(MainActivity.this, para_ti.class);
+
+                startActivity(pt);
+            }
+        });
     }
+
 }
