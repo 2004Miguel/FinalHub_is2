e8f14fff37c571926bc6459bf8b069c4a3334042
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-11-01 12:38:57 -0500

    la app tiene el login y la busqueda. Funcionan correctamente

--- app/src/main/java/com/miguel/final_is2/login.java
+++ app/src/main/java/com/miguel/final_is2/login.java
@@ -2,13 +2,30 @@ package com.miguel.final_is2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button iniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        iniciar=findViewById(R.id.button);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent inicio = new Intent(login.this, MainActivity.class);
+
+                startActivity(inicio);
+            }
+        });
     }
 }
