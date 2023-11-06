ad309c0eb465f0ec326ea6171f3bbcdde7f34f58
Jorge <jorgealejandrozapsan@gmail.com>
2023-11-05 21:45:12 -0500

    Adicción de funcionalidad para registro en el login

--- app/src/main/java/com/miguel/final_is2/login.java
+++ app/src/main/java/com/miguel/final_is2/login.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     Button iniciar;
+    TextView registrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,15 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         iniciar=findViewById(R.id.button);
+        registrarse=findViewById(R.id.textView6);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registro = new Intent(login.this, com.miguel.final_is2.registro.class);
+                startActivity(registro);
+            }
+        });
 
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
